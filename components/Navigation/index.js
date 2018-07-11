2847ab1bbc4e20bf04b5034a3fe5fad408617372
Blake Olsen <blakeolsen@Blakes-MacBook-Pro.local>
2018-07-11 15:30:54 -0500

    Revert "merge"
    This reverts commit 6d72995d6d7aeb6005117287c645585dfa3d13c9.

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -8,53 +8,52 @@ import MerchantScreen from '../../screens/MerchantScreen';
 import CartContainer from '../Cart/CartContainer';
 
 export default Navigator = TabNavigator({
-    CameraScreen: {
-        screen: CameraScreen,
-        navigationOptions: () => ({
-            tabBarIcon: ({tintColor}) => (
-                <Icon
-                    name="camera"
-                    type="font-awesome"
-                    color={tintColor}
-                    size={24}
-                />
-            )
-        })
-    },
-    CartScreen: {
-        screen: CartContainer,
-        navigationOptions: () => ({
-            title: 'Cart',
-            tabBarIcon: ({tintColor}) => (
-                <Icon
-                    name="shopping-cart"
-                    type="font-awesome"
-                    color={tintColor}
-                    size={24}
-                />
-            )
-        })
-    },
-    MerchantScreen: {
-      screen: MerchantScreen,
-      navigationOptions: () => ({
-        tabBarIcon: ({tintColor}) => (
-          <Icon
-              name="shopping-cart"
-              type="font-awesome"
-              color={tintColor}
-              size={24}
-          />
-        )
-      })
-    }
-  }, {
-    tabBarPosition: 'bottom',
-    tabBarOptions: {
-      activeTintColor: '#f2f2f2',
-      activeBackgroundColor: '#2EC4B6',
-      inactiveTintColor: '#666',
-      showLabel: false
-    }
+  CameraScreen: {
+    screen: CameraScreen,
+    navigationOptions: () => ({
+      tabBarIcon: ({tintColor}) => (
+        <Icon
+          name="camera"
+          type="font-awesome"
+          color={tintColor}
+          size={24}
+        />
+      )
+    })
+  },
+  CartScreen: {
+    screen: CartContainer,
+    navigationOptions: () => ({
+      title: 'Cart',
+      tabBarIcon: ({tintColor}) => (
+        <Icon
+          name="shopping-cart"
+          type="font-awesome"
+          color={tintColor}
+          size={24}
+        />
+      )
+    })
+  },
+  MerchantScreen: {
+    screen: MerchantScreen,
+    navigationOptions: () => ({
+      tabBarIcon: ({tintColor}) => (
+        <Icon
+          name="boxes"
+          type="font-awesome"
+          color={tintColor}
+          size={24}
+        />
+      )
+    })
   }
-);
+}, {
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    activeTintColor: '#ffffff',
+    activeBackgroundColor: '#1a1f71',
+    inactiveTintColor: '#000000',
+    showLabel: false
+  }
+});
